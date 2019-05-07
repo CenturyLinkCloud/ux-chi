109c9100982bd8dcb784b61174d4f66aceb1c173
Srinath Koneru <srinath.koneru@centurylink.com>
2019-05-07 07:33:51 -0500

    Update size.ts

--- src/custom-elements/src/utils/size.ts
+++ src/custom-elements/src/utils/size.ts
@@ -1,1 +1,1 @@
-export const VALID_SIZES = ['', 'xs', 'sm', 'sm--2', 'sm--3', 'md', 'lg', 'xl', 'xxl'];
+export const VALID_SPINNER_SIZES = ['', 'xs', 'sm', 'sm--2', 'sm--3', 'md', 'lg', 'xl', 'xxl'];
