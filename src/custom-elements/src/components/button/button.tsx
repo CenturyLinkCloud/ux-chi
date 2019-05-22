42c9018c4d92839136c58f4460dcc1b6b471f4bc
Srinath Koneru <srinath.koneru@centurylink.com>
2019-05-22 16:02:57 -0500

    docs generation

--- src/custom-elements/src/components/button/button.tsx
+++ src/custom-elements/src/components/button/button.tsx
@@ -1,4 +1,4 @@
-import { Component, Event, EventEmitter, Prop, Watch } from '@stencil/core'
+import { Component, Event, EventEmitter, Prop, Watch } from '@stencil/core';
 
 @Component({
   tag: 'chi-button',
