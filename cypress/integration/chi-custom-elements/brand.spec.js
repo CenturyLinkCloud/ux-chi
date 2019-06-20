5e3377ee01d5bf22833eb9b201439bfcb5fae7f3
Srinath Koneru <srinath.koneru@centurylink.com>
2019-06-20 15:53:05 -0500

    adding chi-badge e2e tests

---
+++ cypress/integration/chi-custom-elements/brand.spec.js
@@ -0,0 +1,73 @@
+/// <reference types="Cypress" />
+
+describe('Brand', () => {
+  it('Base brand should be included inside the custom element', () => {
+    cy.visit('tests/custom-elements/brand.html');
+
+    cy.get('[data-cy="brand-base-default"]', { timeout: 5000 })
+      .first()
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('match', 'a.a-brand')
+      .children()
+      .first()
+      .should('have.id', 'brand-base');
+  });
+
+  it('Inverse brand should be included inside the custom element', () => {
+    cy.get('[data-cy="brand-inverse-sm"]', { timeout: 5000 })
+      .first()
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('match', 'a.a-brand')
+      .children()
+      .first()
+      .should('have.id', 'brand-inverse');
+  });
+
+  it('White brand should be included inside the custom element', () => {
+    cy.get('[data-cy="brand-white-md"]', { timeout: 5000 })
+      .first()
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('match', 'a.a-brand')
+      .children()
+      .first()
+      .should('have.id', 'brand-white');
+  });
+
+  it('Black brand should be included inside the custom element', () => {
+    cy.get('[data-cy="brand-black-lg"]', { timeout: 5000 })
+      .first()
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('match', 'a.a-brand')
+      .children()
+      .first()
+      .should('have.id', 'brand-black');
+  });
+
+  it('Brand should have default appropriate class for size', () => {
+    cy.get('[data-cy="brand-black-default"]', { timeout: 5000 })
+      .first()
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('match', 'a.a-brand')
+      .and('not.have.class', '-md');
+  });
+
+  it('Sized brand should have appropriate class', () => {
+    cy.get('[data-cy="brand-white-xxl"]', { timeout: 5000 })
+      .first()
+      .should('have.class', 'hydrated')
+      .children()
+      .first()
+      .should('match', 'a.a-brand.-xxl')
+      .and('not.have.class', 'xxl');
+  });
+});
