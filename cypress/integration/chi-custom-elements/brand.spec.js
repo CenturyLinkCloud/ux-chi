578f66bb0a8230ab240a1f6749b8bb45a427285a
Srinath Koneru <srinath.koneru@centurylink.com>
2019-06-25 11:57:35 -0500

    making requested changes

--- cypress/integration/chi-custom-elements/brand.spec.js
+++ cypress/integration/chi-custom-elements/brand.spec.js
@@ -1,54 +1,52 @@
 describe('Brand', () => {
+  const assertion = (el, value) => {
+    cy.get(el)
+      .children()
+      .first()
+      .should('have.attr', 'data-brand-type')
+      .and('include', value);
+  };
+
+  const selectors = ['brand-base-default', 'brand-inverse-sm', 'brand-white-md', 'brand-black-lg', 'brand-black-default', 'brand-white-xxl'];
+
   before(() => {
     cy.visit('tests/custom-elements/brand.html');
   });
 
   beforeEach(() => {
-    ['brand-base-default', 'brand-inverse-sm', 'brand-white-md', 'brand-black-lg', 'brand-black-default', 'brand-white-xxl'].forEach((s) => {
+    selectors.forEach((s) => {
       cy.get(`[data-cy=${s}]`, { timeout: 5000 })
         .should('have.class', 'hydrated')
         .children()
         .first()
-        .should('match', 'a.a-brand')
+        .should('match', 'span.a-brand')
         .as(s);
     });
   });
 
   it('Base brand should be included inside the custom element', () => {
-    cy.get('@brand-base-default')
-      .children()
-      .first()
-      .should('have.id', 'brand-base');
+    assertion('@brand-base-default', 'base');
   });
 
   it('Inverse brand should be included inside the custom element', () => {
-    cy.get('@brand-inverse-sm')
-      .children()
-      .first()
-      .should('have.id', 'brand-inverse');
+    assertion('@brand-inverse-sm', 'inverse');
   });
 
   it('White brand should be included inside the custom element', () => {
-    cy.get('@brand-white-md')
-      .children()
-      .first()
-      .should('have.id', 'brand-white');
+    assertion('@brand-white-md', 'white');
   });
 
   it('Black brand should be included inside the custom element', () => {
-    cy.get('@brand-black-lg')
-      .children()
-      .first()
-      .should('have.id', 'brand-black');
+    assertion('@brand-black-lg', 'black');
   });
 
   it('Brand should have default appropriate class for size', () => {
     cy.get('@brand-black-default')
-      .and('not.have.class', '-md');
+      .should('not.have.class', '-md');
   });
 
   it('Sized brand should have appropriate class', () => {
     cy.get('@brand-white-xxl')
-      .and('have.class', '-xxl');
+      .should('have.class', '-xxl');
   });
 });
