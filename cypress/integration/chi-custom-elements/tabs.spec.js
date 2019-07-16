db5cddeb0e57810d52d83e5076a172e63aab343f
SergioQ <sergio.quintanacastillo@centurylink.com>
2019-07-16 17:33:10 +0200

    Tests fixes

--- cypress/integration/chi-custom-elements/tabs.spec.js
+++ cypress/integration/chi-custom-elements/tabs.spec.js
@@ -10,13 +10,13 @@ describe('Tabs', function() {
           .first()
           .should('have.class', classToCheck);
     };
+
+    before(function() {
+
+        cy.visit('tests/custom-elements/tabs.html');
+    });
   
     describe('Horizontal Tabs', function() {
-  
-        beforeEach(function() {
-
-            cy.visit('tests/custom-elements/tabs.html');
-        });
 
         it('Tabs custom element should initiate a Chi tabs component. ', () => {
 
@@ -38,50 +38,9 @@ describe('Tabs', function() {
             checkComponent('[data-cy="test-horizontal-bordered-icons"]', '-border');
             checkComponent('[data-cy="test-horizontal-bordered-icons"]', '-icons');
         });
-
-        it('Tabs custom element should change active tab when another one is clicked. ', () => {
-
-            cy.get('[data-cy="test-horizontal"]')
-                .first()
-                .find('li')
-                .as('chi-tabs');
-
-            cy.get('@chi-tabs')
-                .first()
-                .as('first-tab');
-
-            cy.get('@chi-tabs')
-                .eq(1)
-                .as('second-tab');
-
-            cy.get('@first-tab')
-                .should('have.class', '-active');
-
-            cy.get('@second-tab')
-                .click()
-                .should('have.class', '-active');
-        });
     });
   
     describe('Vertical Tabs', function() {
-  
-        beforeEach(function() {
-  
-            cy.visit('tests/custom-elements/tabs.html');
-
-            cy.get('[data-cy="test-vertical"]')
-                .first()
-                .find('li')
-                .as('chi-vertical-tabs');
-
-            cy.get('@chi-vertical-tabs')
-                .first()
-                .as('first-vertical-tab');
-
-            cy.get('@chi-vertical-tabs')
-                .eq(1)
-                .as('second-vertical-tab');
-        });
   
         it('Vertical Tabs custom element should initiate a Chi tabs component. ', () => {
 
@@ -102,16 +61,6 @@ describe('Tabs', function() {
 
             checkComponent('[data-cy="test-vertical-bordered-icons"]', '-border');
             checkComponent('[data-cy="test-vertical-bordered-icons"]', '-icons');
-        });
-
-        it('Vertical Tabs custom element should change active tab when another one is clicked. ', () => {
-
-            cy.get('@first-vertical-tab')
-                .should('have.class', '-active');
-
-            cy.get('@second-vertical-tab')
-                .click()
-                .should('have.class', '-active');
         });
     });
   });
