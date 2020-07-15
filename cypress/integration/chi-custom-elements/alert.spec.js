25354bb0356680438504eb1dcad8f35af6fb7074
Sabah Ahmed <sabah.ahmed@centurylink.com>
2020-07-15 11:32:23 -0600

    [PE-4852] Added no border selectors to banner alert

--- cypress/integration/chi-custom-elements/alert.spec.js
+++ cypress/integration/chi-custom-elements/alert.spec.js
@@ -3,20 +3,21 @@ describe('Alert', () => {
     'alert-banner-success-default',
     'alert-banner-center-success-default',
     'alert-banner-dismissible-success-default',
+    'alert-banner-no-border-success-default',
     'alert-banner-titled-success-default',
+    'alert-banner-actionable-success-default',
     'alert-banner-success-sm',
     'alert-banner-center-success-sm',
     'alert-banner-dismissible-success-sm',
+    'alert-banner-no-border-success-sm',
     'alert-banner-titled-success-sm',
+    'alert-banner-actionable-success-sm',
     'alert-banner-success-lg',
     'alert-banner-center-success-lg',
     'alert-banner-dismissible-success-lg',
     'alert-banner-no-border-success-lg',
     'alert-banner-titled-success-lg',
-    'alert-banner-actionable-success-default',
-    'alert-banner-actionable-success-sm',
     'alert-banner-actionable-success-lg'
-
   ];
 
   const alertBubbleSelectors = [
