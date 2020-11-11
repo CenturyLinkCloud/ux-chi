cf6fb3cdf552c3170106f92514bfda04916ccd63
Jani Bolkvadze <jani.bolkvadze@gmail.com>
2020-11-11 13:46:20 +0100

    [FSPE-5297] Improved focusVisibility function triggering

--- tasks/build-chi-scripts.js
+++ tasks/build-chi-scripts.js
@@ -69,7 +69,7 @@ function buildChiScripts () {
     .pipe(plumber())
     .pipe(vinylNamed())
     .pipe(webpackStream(webpackConfig))
-    .pipe(footer("(function() {document.addEventListener('DOMContentLoaded', function() {const NO_FOCUS_CLASS = '-no-focus-outline';document.body.classList.add(NO_FOCUS_CLASS);window.addEventListener('keyup', function (e) {if (e.which === 9) {document.body.classList.remove(NO_FOCUS_CLASS);}});Array.prototype.forEach.call(document.querySelectorAll('a, input, button'),function(focusableElement) {focusableElement.addEventListener('blur', function (e) { if (!e.relatedTarget) {document.body.classList.add(NO_FOCUS_CLASS);}});});})})();"))
+    .pipe(footer("(function() {window.addEventListener('load', function() {const NO_FOCUS_CLASS = '-no-focus-outline';document.body.classList.add(NO_FOCUS_CLASS);window.addEventListener('keyup', function (e) {if (e.which === 9) {document.body.classList.remove(NO_FOCUS_CLASS);}});Array.prototype.forEach.call(document.querySelectorAll('a, input, button'),function(focusableElement) {focusableElement.addEventListener('blur', function (e) { if (!e.relatedTarget) {document.body.classList.add(NO_FOCUS_CLASS);}});});})})();"))
     .pipe(gulp.dest(destination));
 }
 
